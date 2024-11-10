563ae5e53f428f54b132fc1be3d8fe8789c144bd
nahid <nahidahmdv99@gmail.com>
2024-11-11 00:54:38 +0330

    a new project was created and some models have been added.

---
+++ store/admin.py
@@ -0,0 +1,20 @@
+#here we just need to tell Django what models we want to use (register them) so that we can start adding data to those models. so we just need to go ahead and describe what we want to include.
+from django.contrib import admin
+from .models import Category, Product
+# Register your models here.
+
+@admin.register(Category) #This decorator is gonna tell Django that we want to register 'Category' model with the Django admin site, linking it to the 'CategoryAdmin' class, allowing it to be managed through the admin interface. in the class below we add some additional information.
+class CategoryAdmin(admin.ModelAdmin): #This defines a custom admin class for the 'Category' model, inheriting from 'admin.ModelAdmin'. This allows for customization of how the model is displayed in the admin area.
+    #in the following lines I'm gonna tell Django that I want to display things in a very specific way in my admin area:
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}#پر می‌شود slug وقتی یک کتگوری بخواهیم وارد کنیم، به محض وارد کردن اسمش خودبه‌خود فیلد مربوط به #This attribute allows automatic generation of certain fields based on other fields. Here, it automatically populates the 'slug' field based on the value entered in the 'name' field.
+
+
+@admin.register(Product) #This decorator registers the 'Product' model with a custom admin class, 'ProductAdmin', which defines how this model will be displayed and interacted with in the admin panel.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price',
+                    'in_stock', 'created_at', 'updated_at']
+    list_filter = ['in_stock', 'is_active'] # This attribute adds filter options on the right side of the list view.
+    list_editable = ['price', 'in_stock'] #This attribute allows certain fields to be edited directly in the list view without needing to go into each individual product's detail page. Here, both 'price' and 'in_stock' can be edited inline.
+    prepopulated_fields = {'slug': ('title',)}
+#Just remember that in admin area I need 'Categories' to be able to add new 'Products'. I'm not allowed to add a product without a category.
---
+++ store/models.py
@@ -0,0 +1,111 @@
+from django.db import models
+from django.contrib.auth.models import User #This is the user model that Django automatically builds.
+from django.urls import reverse
+# Create your models here.
+class Category(models.Model): #we're extending from 'models.Model'. That's gonna provide us access to kind of the functionality that we're gonna need to build and describe a model.
+    #things we need to record about a category:
+    name = models.CharField(max_length=255, db_index=True) #the datatype of our 'name' field is character field. 'db_index=True' is used to create an index on that field in the database. 
+    #Multiple records can share the same value in a non-unique indexed field (like 'name'), but each record will still have a distinct record identified by their unique primary key (ID).
+    slug = models.SlugField(max_length=255, unique=True) #In order to get to a specific category we want to type into the browser  '127.0.0.1:8000/the-slug-of-that-category/'.
+
+    class Meta: #data about data. The 'Meta' class is an inner class that provides metadata options for the model. Metadata is anything that’s not a field, such as ordering options, database table names, and human-readable names.
+        verbose_name_plural = 'categories' #This attribute in the 'Meta' class of a Django model is primarily used for displaying the plural name of that model in the Django administration interface (admin site) and other user-facing contexts. This means when you are managing this model in the admin interface, it will show "Categories" instead of a default pluralization "Categorys".
+    '''
+    While one of the primary uses of the Meta class is to configure how models are displayed in the Django admin area (through options like verbose_name and verbose_name_plural), 
+    it also serves many other purposes related to database behavior, relationships, and constraints.
+    '''
+    
+    def get_absolute_url(self): #This method is typically included in a Django model and is used to return the canonical URL for an instance of that model. It allows you to easily retrieve the URL for an object without needing to know its specific path.
+        return reverse('store:category_list', args=[self.slug])
+    
+    '''
+    reverse('store:category_list', ...):
+      The 'reverse()' function takes the name of the URL pattern as its first argument. In this case, 'store:category_list' refers to a URL pattern defined in your 'urls.py' file under the namespace 'store' named 'category_list'. This pattern should correspond to a view that handles displaying list of categories.
+    args=[self.slug]:
+      The 'args' parameter is used to pass positional arguments to the URL. Here, 'self.slug' refers to the slug attribute of the model instance (e.g., a unique identifier for the category). This slug will be inserted into the URL where needed, allowing Django to construct the correct path for that specific category.
+    example:When you call 'some_category.get_absolute_url()', where 'some_category' is an instance of 'Category' model, it will return a URL like '/store/my-first-category/', assuming the slug for that category is 'my-first-category'.'''
+    def __str__(self): #dunder string method
+        return self.name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE) #by defining this foreign key field we build a link between
+    '''
+    class Product(models.Model)::
+        This line defines a new Django model called 'Product', which inherits from 'models.Model'. This inheritance means that 'Product' will have all the functionalities provided by Django’s ORM (Object-Relational Mapping) system, allowing you to interact with the database easily.
+    category = models.ForeignKey(...):
+        The category field is defined as a foreign key. A foreign key is a field that creates a many-to-one relationship between two models. In this case, each product can be associated with one category, but each category can have multiple products.
+        The foreign key points to the 'Category' model, indicating that each product is linked to a specific category.
+    Parameters of ForeignKey:
+        Category: This specifies the model that the foreign key points to. In this case, it references the 'Category' model.
+        related_name='product': This parameter defines the name of the reverse relation from the 'Category' model back to the 'Product'. When you access a category instance, you can use this related name to get all products associated with that category.
+        For example, if you have a category instance called 'electronics', you can retrieve all products in that category using:
+        python
+        electronics.product.all()
+    on_delete=models.CASCADE: This parameter specifies what should happen when the referenced category is deleted. The CASCADE option means that if a category is deleted, all associated products will also be deleted automatically. This helps maintain referential integrity in your database by ensuring there are no orphaned product records.
+    '''
+    created_by = models.ForeignKey(User, related_name='product_creator', on_delete=models.CASCADE, default='admin') #I want to record who actually made that data so I made a foreign key to the Django default 'User' table. so we need to add 'User' table into our database, so we need to import that model in (line 2).
+    title = models.CharField(max_length=255) #The title of the product.
+    author = models.CharField(max_length=255, default='admin') #The author of the book.
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/') #(we should have Pillow (which is a package that helps us manage images) installed) I'm gonna assume that the product only has one image. we're not actually storing the image in the database; we're storing the link of the image to the database. So we're gonna have to set up that link system (steps 2 and 3 below).
+    #we just have to create a new folder called 'media' in the main root of our project. we don't need to also create 'images' folder inside of this 'media' folder, this is just gonna create automatically and Django is gonna do that for us.
+    '''
+    image:
+        This is the name of the field in the model. It will be used to reference the uploaded image in instances of this model.
+    models.ImageField:
+        ImageField is a specialized field type in Django that inherits from FileField. It is specifically intended for storing image files. When you use ImageField, Django performs checks to ensure that the uploaded file is a valid image format (e.g., JPEG, PNG, GIF).
+    upload_to='images/':
+        The 'upload_to' parameter specifies the directory within your media root where uploaded images will be stored. In this case, images will be saved in a folder named 'images/' in your media root.
+        This path is relative to the 'MEDIA_ROOT' setting in your Django project, which defines the base directory for media files.
+    How It Works
+     When a user uploads an image through a form that uses this model, Django handles the file upload process as follows:
+       Storage: The uploaded image file is saved to the specified directory (images/) under the 'MEDIA_ROOT'.
+       Database Entry: The path to the uploaded image file is stored in the database, allowing you to retrieve it later.
+       Validation: Django uses the Pillow library (which must be installed) to validate that the uploaded file is indeed an image and can also handle various image processing tasks.
+    Additional Considerations
+    1)Pillow Library: To use ImageField, you need to have the Pillow library installed.
+    2)MEDIA_URL and MEDIA_ROOT:You should configure your settings.py to define 'MEDIA_URL' and 'MEDIA_ROOT'. For example:
+            MEDIA_URL = '/media/'
+            MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+            This configuration allows Django to serve media files during development.
+    3)Serving Media Files:In development, you can serve media files using Django's built-in server by adding URL patterns in your urls.py:
+            from django.conf import settings
+            from django.conf.urls.static import static
+  
+            urlpatterns = [
+                # Your other URL patterns
+            ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    4)Accessing Uploaded Images:In templates, you can access the uploaded image URL like this:
+            xml
+            <img src="{{ instance.image.url }}" alt="Product Image">
+            Here, instance refers to an instance of your model that contains the uploaded image.
+    For example, if you set:
+    python
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+    And you have an image uploaded to 'images/', the actual file path on your server would be something like:
+    text
+    /path/to/your/project/media/images/your_image.jpg
+    '''
+    slug = models.SlugField(max_length=255) #In order to get to a specific product we want to type into the browser '127.0.0.1:8000/the-slug-of-that-product/'.
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True) #There might be some products that aren't actually active to buy, maybe for example you've run out of stock.
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    '''
+    If you set 'auto_now=True', every time you save an instance of that model, the timestamp will update.
+    If you set 'auto_now_add=True', it will only set the timestamp once at creation, and any subsequent saves will not affect this field.
+    '''
+    # objects = models.Manager()
+    # products = ProductManager()
+    class Meta:
+        verbose_name_plural = 'Products' #unnecessary
+        ordering = ('-created_at',)  #when we return all the data from 'Product' table we can specify what order we want that data to be in. Here we're ordering our list that gets returned from the database in descending order. It means that for example the last item that is added to the database is shown first.
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+
+    def __str__(self):
+        return self.title
